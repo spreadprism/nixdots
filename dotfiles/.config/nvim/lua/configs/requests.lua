f1b0316dd0c055421a00773fbcaef940e567e447
Emeric Duguay <emeric@everflow.io>
2025-03-04 12:30:48 -0500

    kualala configs

--- dotfiles/.config/nvim/lua/configs/requests.lua
+++ dotfiles/.config/nvim/lua/configs/requests.lua
@@ -1,2 +1,8 @@
 -- TODO: Add kulala-ls
-plugin("mistweaverco/kulala.nvim"):opts({}):event("VeryLazy")
+plugin("mistweaverco/kulala.nvim")
+	:opts({
+		global_keymaps = false,
+		vscode_rest_client_environmentvars = true,
+		debug = true,
+	})
+	:event("VeryLazy")
--- dotfiles/.config/nvim/lua/keybinds/request.lua
+++ dotfiles/.config/nvim/lua/keybinds/request.lua
@@ -1,5 +1,5 @@
 keybind_group("<leader>r", "requests"):register({
-	keybind("n", "r", function()
+	keybind({ "n", "v" }, "r", function()
 		require("kulala").run()
 	end, "Run the current request"),
 	keybind("n", "R", function()
@@ -8,4 +8,24 @@ keybind_group("<leader>r", "requests"):register({
 	keybind("n", "l", function()
 		require("kulala").replay()
 	end, "Run the last request"),
+	keybind("n", "O", function()
+		require("kulala").open()
+	end, "Open float"),
+	keybind("n", "o", function()
+		-- new tab
+		vim.cmd("tabnew")
+		require("kulala").scratchpad()
+	end, "Open scratchpad"),
+	keybind("n", "d", function()
+		require("kulala").download_graphql_schema()
+	end, "Download GraphQL schema"),
+	keybind("n", "x", function()
+		require("kulala").scripts_clear_global()
+	end, "Clear global scripts"),
+	keybind("n", "i", function()
+		require("kulala").inspect()
+	end, "Inspect current request"),
+	keybind("n", "s", function()
+		require("kulala").show_stats()
+	end, "Last request stats"),
 })
