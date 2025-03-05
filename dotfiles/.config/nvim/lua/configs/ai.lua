e4e1047673d1b4c5b084557177d9ac1171807f32
Emeric Duguay <emeric@everflow.io>
2025-03-05 14:12:13 -0500

    commit changes

--- dotfiles/.config/nvim/lua/configs/ai.lua
+++ dotfiles/.config/nvim/lua/configs/ai.lua
@@ -4,57 +4,60 @@
 
 local use_copilot = true
 local copilot = plugin("zbirenbaum/copilot.lua")
-	:event("VeryLazy")
-	:enabled(use_copilot) -- INFO: Don't have the subscription anymore
-	:opts({
-		suggestion = {
-			enabled = true,
-			auto_trigger = false,
-			keymap = {
-				accept = "<M-a>",
-				dismiss = "<M-d>",
-				next = false,
-			},
-		},
-		panel = { enabled = false },
-	})
+  :event("VeryLazy")
+  :enabled(use_copilot) -- INFO: Don't have the subscription anymore
+  :opts({
+  suggestion = {
+    enabled = true,
+    auto_trigger = false,
+    keymap = {
+      accept = "<M-a>",
+      dismiss = "<M-d>",
+      next = false,
+    },
+  },
+  panel = { enabled = false },
+})
 
 local GetChatAdapter = function()
-	if os.getenv("GEMINI_API_KEY") ~= nil then
-		return "gemini"
-	else
-		return "copilot"
-	end
+  if os.getenv("GEMINI_API_KEY") ~= nil then
+    return "gemini"
+  else
+    return "copilot"
+  end
 end
 plugin("olimorris/codecompanion.nvim")
-	:dependencies({
-		copilot,
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-	})
-	:event("VeryLazy")
-	:opts({
-		display = {
-			chat = {
-				window = {
-					layout = "float",
-					border = "rounded",
-					height = 0.8,
-					width = 0.8,
-				},
-			},
-		},
-		strategies = {
-			chat = {
-				adapter = GetChatAdapter(),
-				keymaps = {
-					close = {
-						modes = { i = "<C-q>" },
-					},
-				},
-			},
-			inline = {
-				adapter = GetChatAdapter(),
-			},
-		},
-	})
+  :dependencies({
+    copilot,
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  })
+  :event("VeryLazy")
+  :opts({
+  display = {
+    diff = {
+      provider = "mini_diff",
+    },
+    chat = {
+      window = {
+        layout = "float",
+        border = "rounded",
+        height = 0.8,
+        width = 0.8,
+      },
+    },
+  },
+  strategies = {
+    chat = {
+      adapter = GetChatAdapter(),
+      keymaps = {
+        close = {
+          modes = { i = "<C-q>" },
+        },
+      },
+    },
+    inline = {
+      adapter = GetChatAdapter(),
+    },
+  },
+})
