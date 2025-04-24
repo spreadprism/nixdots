669e685ae71b5fc02db25f6626338d910773c506
spreadprism <sprism@proton.me>
2025-04-24 17:41:10 -0400

    begining for codelldb

--- overlays/default.nix
+++ overlays/default.nix
@@ -3,6 +3,9 @@
   # This one brings our custom packages from the 'pkgs' directory
   additions = final: _prev: import ../pkgs final.pkgs;
 
+  # codelldb = final: prev: {
+  #     codelldb = prev.vscode-extensions.vadimcn.vscode-lldb;
+  # };
   # When applied, the unstable nixpkgs set (declared in the flake inputs) will
   # be accessible through 'pkgs.unstable'
   stable-packages = final: _prev: {
